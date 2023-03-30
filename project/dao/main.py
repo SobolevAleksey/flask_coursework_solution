33fd091e69de6844ec8525f88e392582ccb1f4ad
Aleksey Sobolev <113844347+SobolevAleksey@users.noreply.github.com>
2023-03-30 11:44:31 +0300

    Update main.py

--- project/dao/main.py
+++ project/dao/main.py
@@ -5,5 +5,16 @@ from project.models import Genre
 class GenresDAO(BaseDAO[Genre]):
     __model__ = Genre
 
+ # Разбить на разные файлы    
+class DirectorsDAO(BaseDAO[Director]):
+    __model__ = Director
+
+class UsersDAO(BaseDAO[User]):
+    __model__ = User
+
+class MoviesDAO(BaseDAO[Movie]):
+    __model__ = Movie
+
+
 
 
