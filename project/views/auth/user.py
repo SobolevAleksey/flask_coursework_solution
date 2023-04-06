29cc3e8f0a52730e21f88016342ab05dd52fbb34
Aleksey Sobolev <113844347+SobolevAleksey@users.noreply.github.com>
2023-04-06 16:50:18 +0300

    Update user.py

--- project/views/auth/user.py
+++ project/views/auth/user.py
@@ -2,21 +2,30 @@ from flask_restx import Namespace, Resource
 from project.setup.api.models import user
 from project.container import users_service
 
+# Реализуем следующие эндпоинты:
+
+#- **GET** /user/ — получить информацию о пользователе (его профиль).
+#- **PATCH** /user/ — изменить информацию пользователя (имя, фамилия, любимый жанр).
+#- **PUT** /user/password — обновить пароль пользователя, для этого нужно отправить два пароля *password_1* и *password_2.*
+
+#Для того чтобы все  ссылки корректно работали, их нужно обернуть в декоратор, в котором мы будет проверять переданный токен.
+
 user_ns = Namespace('user')
 
 @user_ns.route('/')
 class UserView(Resource):
-    @user_ns.expect(user)
-    #@user_ns.response(201, description='OK')
-    def get(self):
-        pass
-    def patch(self):
-        pass
+    def get(self, user_id):
+        return user_service.get_one(user_id)
+        
+    def patch(self,user_d):
+        return user_service.update(user_d)
+        
 
 
 
-@user_ns.route('/password/')
-class UserView(Resource):
-    def put(self):
+#@user_ns.route('/password/')
+#class UserView(Resource):
+    def put(self, password_1 and password_2):
+        data = request.json
         pass
 
